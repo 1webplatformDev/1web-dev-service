0c15b2b324e1f85e7c99762f53f676a6dd979952
maks <maks>
2023-10-27 20:31:57 +0400

    типизация генерация sql

--- src/sql-generator/interface/sql-generator-table.interface.ts
+++ src/sql-generator/interface/sql-generator-table.interface.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { SqlGeneratorTableColumnInterface } from "./sql-generator-table-column.interface";
 
 export class SqlGeneratorTableInterface {
   @ApiProperty()
   name: string;
+
   @ApiProperty()
   comment: string;
+
+  @ApiProperty()
+  column: SqlGeneratorTableColumnInterface[];
 }
